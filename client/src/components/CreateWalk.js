976fe35d4dd20534a8e6907bd4a31659f9696ef7
Sofia Katzman <katzmansof@gmail.com>
2023-07-25 21:30:57 -0400

    add spotted bunnies data to walk creation

--- client/src/components/CreateWalk.js
+++ client/src/components/CreateWalk.js
@@ -11,27 +11,19 @@ function CreateWalk({bunnies, setBunnies}) {
   const [startTime, setStartTime] = useState("")
   const [paths, setPaths] = useState(null)
   const [count, setCount] = useState(0)
-  const [spottedBunnies, setSpottedBunnies] = useState([]);
+  const [spottedBunnies, setSpottedBunnies] = useState([])
 
-
+  const [savedWalk, setSavedWalk] = useState(null)
 
   useEffect(() => {
     fetch("/paths")
         .then(r => r.json())
-        .then(data =>{
-          const random = {
-            directions: "-",
-            id: 0,
-            name: "random"
-          }
-          setPaths([...data, random])}
-          )
+        .then(data => setPaths(data))
   }, [])
 
   // Form validation schema
   const formSchema = yup.object().shape({
     walk_path: yup.number().required('You must select a path.'),
-    bunny_count: yup.number().positive('You must save at least one bunny to your walk.'),
   })
 
   const formik = useFormik({
@@ -40,14 +32,27 @@ function CreateWalk({bunnies, setBunnies}) {
       start_time: "",
       end_time: "",
       bunny_count: "",
-      walk_path: ""
+      walk_path: "",
+      spotted_bunnies: ""
     },
     validationSchema: formSchema,
+    validate: (values) => {
+      const errors = {};
+
+      // Calculate bunny_count based on the spottedBunnies array length
+      const bunny_count = spottedBunnies.length;
+      if (bunny_count === 0) {
+        errors.bunny_count = "You must save at least one bunny to your walk.";
+      }
+
+      return errors;
+    },
     onSubmit: (values) => {
       values.end_time = moment().format('LT')
       values.start_time = startTime
       values.bunny_count = count
       values.date = date
+      values.spotted_bunnies = spottedBunnies
           
       fetch('/walks', {
         method: 'POST',
@@ -59,7 +64,7 @@ function CreateWalk({bunnies, setBunnies}) {
       .then((res) => {
         if (res.ok) {
           res.json().then((response) => {
-            console.log(response) 
+            setSavedWalk(response) 
           })
         } else {
           console.error("Failed to save the walk.")
@@ -68,6 +73,8 @@ function CreateWalk({bunnies, setBunnies}) {
       .catch((error) => {
         console.error("Error occurred while saving the walk:", error)
       })
+      setCount(0)
+      setSpottedBunnies([])
       setWalk(!walk)
     },
   })
@@ -86,44 +93,59 @@ function CreateWalk({bunnies, setBunnies}) {
   }
 
   // Display when a walk has not been started
+  if(!walk && savedWalk){
+    console.log(savedWalk)
+    return(
+      <div className="walk-card"> 
+      <h1>Walk added!</h1>
+      <p>Bunnies Spotted: {savedWalk.bunny_count}</p>
+      <p>Time Started: {savedWalk.start_time}</p>
+      <p>Time Ended: {savedWalk.end_time}</p>
+
+
+      <button onClick={()=>setSavedWalk(null)}>exit</button>
+      </div>
+    )
+  }
+
+
+
   if (!walk) {
     return (
-      <div className="container"> <br/><br/><br/>
-      new walk!
-        <button onClick={handleStartWalk}>Start Walk</button> <br/>
+      <div className="new-walk"> <br/><br/>
+              <button onClick={handleStartWalk}>Start Walk</button> <br/>
       </div>
     )
   }
 
   // Display when a walk has been started
   return (
-    <div className="container-seen-bunnies"> <br/>
-      <h2>{count} bunnies seen!</h2>
+    <div className="new-walk"> <br/>
+      <h2 >{count} bunnies seen!</h2>
     
-    {bunnies && <div>
+    {bunnies && <div className="container-seen-bunnies">
         
-        saved bunnies:
-        click on any of these bunnies when you see them to add them to the
+        
+        <p>click on any of these bunnies when you see them to add them to the tally! <br/>
+
+        spot a new bunny? use the "random bunny" button!       
+        </p>
         {bunnies.map(bunny => {
             return(
                 //will need to add css to this so they show very small 
-                <div key={bunny.id}>
-                <img src={bunny.headshot} alt={`Headshot of ${bunny.name}`} />
-                <h5>{bunny.name}</h5>
-                <h6>{bunny.description}</h6>
-                
-                <button onClick={() => handleAddBunny(bunny.id)}>spotted</button>
+                <div className="card-bunnies-seen" key={`bunny${bunny.id}`} onClick={() => handleAddBunny(bunny.id)}>
+                  <img className="card-bunnies-image-seen" src={bunny.headshot} alt={`Headshot of ${bunny.name}`} />
+                  <h5 className="name-bunnies-seen">{bunny.name}</h5>
+  
                 </div>
             )
         })}
-        <h5>Random Bunny</h5>
-                <img src="https://i.guim.co.uk/img/media/a6478477c7508115778a257a5011570f66032941/0_85_2048_1229/master/2048.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=afea1b033d076492c371b95545ba089f" alt={`Random Bunny Placeholder`} />
-                <button onClick={() => handleAddBunny(0)}>spotted</button>     
+          
         </div>}
           
    
-      <h3>walk date: {date}</h3>
-      <h3>walk start time: {startTime}</h3>
+      <p>walk date: {date} <br/>
+      walk start time: {startTime}</p>
       <form onSubmit={formik.handleSubmit}>
         <select
           name="walk_path"
@@ -132,20 +154,18 @@ function CreateWalk({bunnies, setBunnies}) {
         >
           <option value="">Select a path</option>
           {paths && paths.map((path) => (
-            <option key={path.id} value={path.id}>
+            <option key={`path${path.id}`} value={path.id}>
               {path.name.toLowerCase()}
             </option>
           ))}
         </select>
-
-
 
         {/* formik error handling in case there were issues storing values -- shouldnt be triggered */}
         {formik.errors && (
           <div className="errors">
             <ul>
               {Object.values(formik.errors).map((error, index) => (
-                <h6 key={index} style={{ color: 'red' }}>{error}</h6>
+                <h6 key={`error${index}`} style={{ color: 'red' }}>{error}</h6>
               ))}
             </ul>
           </div>
